d3b9a71649a280e83a014d208d7240222045176a
sukho <sukho8757@gmail.com>
2023-02-14 08:24:14 +0900

    URL chacker with goroutines

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
-var errRequestFailed = errors.New("Request Failed")
+type requestResult struct {
+	url    string
+	status string
+}
 
 func main() {
-	var results = make(map[string]string)
-
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -24,25 +26,23 @@ func main() {
 	}
 
 	for _, url := range urls {
-		result := "OK"
-		err := hitUrl(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitUrl(url, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, "  ", result)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
 // Hit the URL
-func hitUrl(url string) error {
-	fmt.Println("Checking ", url)
+func hitUrl(url string, c chan<- requestResult) {
+	status := "OK"
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
