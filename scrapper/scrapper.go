70b2b33dc027c7a025dd2f0456761e09cb5518e6
sukho <sukho8757@gmail.com>
2023-02-14 12:11:57 +0900

    Job scraper with goroutines complete

---
+++ scrapper/scrapper.go
@@ -0,0 +1,122 @@
+package scrapper
+
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type extractedJob struct {
+	id        string
+	title     string
+	condition string
+}
+
+func Scrape(term string) {
+	var baseUrl string = "https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=" + term + "&recruitPageCount=10"
+	var jobs []extractedJob
+	c := make(chan []extractedJob)
+	totalPages := getPages(baseUrl)
+
+	for i := 0; i < totalPages; i++ {
+		go getPage(baseUrl, i, c)
+
+	}
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-c
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	whiteJobs(jobs)
+}
+
+func whiteJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+
+	defer w.Flush()
+
+	headers := []string{"LINK", "TITLE", "CONDITION"}
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://www.saramin.co.kr/zf_user/jobs/relay/view?rec_idx=" + job.id, job.title, job.condition}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+}
+
+func getPage(baseUrl string, pageNumber int, mainC chan<- []extractedJob) {
+	var jobs []extractedJob
+	c := make(chan extractedJob)
+	pageURL := baseUrl + "&recruitPage=" + strconv.Itoa(pageNumber+1)
+	fmt.Println("requesting: ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		go extractJob(card, c)
+	})
+
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	mainC <- jobs
+}
+
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
+	id, _ := card.Attr("value")
+	title := CleanString(card.Find(".job_tit>a").Text())
+	condition := CleanString(card.Find(".job_condition").Text())
+	c <- extractedJob{id: id, title: title, condition: condition}
+}
+
+// CleanString cleans string
+func CleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func getPages(baseUrl string) int {
+	pages := 0
+	res, err := http.Get(baseUrl)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
